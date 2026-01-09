75e5e534ac6727230b9809827590bc33787146ba
xuanfeiren <xuanfeir@gmail.com>
2026-01-09 15:54:48 -0600

    update

--- KernelBench/guide/test_client.py
+++ KernelBench/guide/test_client.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from cuda_eval_client import CUDAEvalClient
 
 client = CUDAEvalClient("http://localhost:6000")
