dc797f71ce1f25aee8b60daee184798dd1ba4b1a
xuanfeiren <xuanfeir@gmail.com>
2026-01-09 15:54:03 -0600

    add a test_client

---
+++ KernelBench/guide/test_client.py
@@ -0,0 +1,21 @@
+from cuda_eval_client import CUDAEvalClient
+
+client = CUDAEvalClient("http://localhost:6000")
+problem_id = 1 
+sample_id = 0
+from datasets import load_dataset
+ds = load_dataset("allenanie/kernelbench_with_prompts")
+examples = [ex for ex in ds['train'] if ex['backend'] == 'cuda']
+ex1 = examples[0]
+ref_arch_src = ex1['ref_arch_src']
+custom_cuda = open("level1_prob1_cuda_custom_cuda_gpt5_example.txt").read()
+
+job_id = client.submit_evaluation(
+            problem_id=problem_id, sample_id=sample_id,
+            custom_cuda=custom_cuda,
+            ref_arch_src=ref_arch_src,
+            level=1
+)
+
+result = client.wait_for_job(job_id, timeout=300)
+kernel_exec_result = result['result']
